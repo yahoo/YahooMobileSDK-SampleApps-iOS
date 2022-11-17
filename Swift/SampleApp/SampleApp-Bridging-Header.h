7ada82417e044c70c1cf0c126726275e3577b798
Alexandru Astilean <alexandru.astilean@yahooinc.com>
2022-11-17 20:05:34 +0200

    Update the Yahoo Mobile SDK dependency to 1.3.0.

--- Swift/SampleApp/SampleApp-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-///
-/// Use this file to import your target's public headers that you would like to expose to Swift.
-///
-/// @copyright Copyright Yahoo, Licensed under the terms of the Apache 2.0 license . See LICENSE file in project root for terms.
-///
-
-#import <YahooAds/YahooAds.h>
